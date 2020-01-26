3fcfca1547ac2b7d34fd1368bcc7310387065f77
Daniel Cregg <daniel.cregg@gmit.ie>
2020-01-26 22:09:37 +0000

    Initial Commit

---
+++ src/ie/gmit/lectweek2/Test.java
@@ -0,0 +1,18 @@
+package ie.gmit.lectweek2;
+
+class Test {
+
+    public static void main(String[] args) {
+        // Print Hello World! to the console.
+        // System.out.println("Hello World!");
+
+        for (int i = 0; i < 10; i++) {
+            mymethod();
+        }
+    }
+
+    public static void mymethod() {
+        System.out.println("My name is Daniel");
+    }
+
+}
---
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -0,0 +1,10 @@
+package ie.gmit.packagename;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+        // Print Hello World! to the console.
+        System.out.println("Hello World!");
+    }
+
+}
